ce3076e7e760e0b49dd5c1392b128edf7e83bee4
sasank <kotasasank123@gmail.com>
2026-04-10 14:40:41 +0530

    fixed redirect uri

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -9,7 +9,7 @@ const {
 const oauth2Client = new google.auth.OAuth2(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
-  "http://localhost:5000/auth/google/callback"
+  `${process.env.BASE_URL}/auth/google/callback`
 );
 
 // Step 1: Login
